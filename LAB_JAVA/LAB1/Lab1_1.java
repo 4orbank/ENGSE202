e4c0387bad6121864f915b6fdc11b113707d0d9b
Bank <tanakhanluenam@gmail.com>
2025-11-22 21:29:51 +0700

    Add files via upload

---
+++ LAB_JAVA/LAB1/Lab1_1.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();  // รับค่าตัวแรก
+        int b = sc.nextInt();  // รับค่าตัวที่สอง
+
+        int sum = a + b;       // คำนวณผลบวก
+
+        System.out.println(sum);  // แสดงผลลัพธ์
+	}
+
+}
