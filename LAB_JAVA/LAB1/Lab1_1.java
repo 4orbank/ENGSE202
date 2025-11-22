3432a9c13d46026b7b61d1bb3ccf0b0f63586d6a
Bank <tanakhanluenam@gmail.com>
2025-11-22 21:47:20 +0700

    Update Lab1_1.java

--- LAB_JAVA/LAB1/Lab1_1.java
+++ LAB_JAVA/LAB1/Lab1_1.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class Lab1_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();  // รับค่าตัวแรก
-        int b = sc.nextInt();  // รับค่าตัวที่สอง
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
-        int sum = a + b;       // คำนวณผลบวก
+        int totalallnum = num1 + num2;
 
-        System.out.println(sum);  // แสดงผลลัพธ์
+        System.out.println(totalallnum);
 	}
 
 }
