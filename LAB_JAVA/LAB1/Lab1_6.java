6d254a7a0e954b066949902233e01848a65f814b
Bank <tanakhanluenam@gmail.com>
2025-12-02 03:50:11 +0700

    Add files via upload

---
+++ LAB_JAVA/LAB1/Lab1_6.java
@@ -0,0 +1,18 @@
+package Lab1;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Lab1_6 {
+	public static void main(String[] args) {
+		Scanner math = new Scanner(System.in);
+		
+		System.out.print("Enter your Number : ");
+		int number =  math.nextInt();
+		
+		for (int i = 1; i <= 12; i++) {
+			int result = number * i;
+			System.out.println(number +  "x" + i + "=" + result);
+		}
+		math.close();
+		}
+}
