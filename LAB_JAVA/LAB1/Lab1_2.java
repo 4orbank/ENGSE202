605db743a1ce9fb8fdc412081e597e9f3d6a84e6
Bank <tanakhanluenam@gmail.com>
2025-12-02 02:56:33 +0700

    Update Lab1_2.java

--- LAB_JAVA/LAB1/Lab1_2.java
+++ LAB_JAVA/LAB1/Lab1_2.java
@@ -1,16 +1,19 @@
 package Lab1;
-
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Lab1_2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner getnumber = new Scanner(System.in);
+		
+		System.out.println("Input Width : ");
+		float width = getnumber.nextFloat();
+		
+		System.out.println("Input Height : ");
+		float height = getnumber.nextFloat();
+		
+		float area = width * height;
+		System.out.println("Your Area =" + area);
 
-        double width = sc.nextDouble();    // รับค่าความกว้าง
-        double height = sc.nextDouble();   // รับค่าความสูง
-
-        double area = width * height;      // คำนวณพื้นที่
-
-        System.out.println(area);          // แสดงผลลัพธ์
-    }
+	}
 }
