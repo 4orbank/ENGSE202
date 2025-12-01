70d1dfe1814a9cd7d1ccab670f0e99de8ec23ddb
Bank <tanakhanluenam@gmail.com>
2025-12-02 03:28:28 +0700

    Update lab1_4.java

--- LAB_JAVA/LAB1/lab1_4.java
+++ LAB_JAVA/LAB1/lab1_4.java
@@ -1,22 +1,32 @@
+package Lab1;
 import java.util.Scanner;
-public class lab1_4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+import java.util.InputMismatchException;
 
-
-        int kanam;
-        System.out.print("ใส่คะแนนอันน้อยนิดของมึงมา: ");        kanam = scanner.nextInt();
-        if (kanam >= 80) {
-            System.out.println("พ่อคนเก่ง A");
-        } else if (70 >= kanam && kanam <= 79) {
-            System.out.println("ฉลาดหนิได้ B");
-        } 
-        else if (60 >= kanam && kanam <= 69) {
-            System.out.println("เกือบฉลาดได้ C");
-        } else if (50 >= kanam && kanam <= 59) {
-            System.out.println("เกือบโง่ได้ D");
-        } else {
-            System.out.println("ติด F ไปดิไอ่โง่");
-        }
-    }
+public class Lab1_4 {
+	public static void main(String[] args) {
+		Scanner getscore = new Scanner (System.in);
+		
+	System.out.println("Pls Input Score : ");
+	int score = getscore.nextInt();
+	
+	if (score < 0 || score > 100) {
+		System.out.println("คะแนนบ้านพ่อมึงน้อยกว่า 0 เยอะกว่า 100 ");
+	}
+	else if (score < 50){
+		System.out.println("ติดF");
+	}
+	else if (score >= 50 && score <= 59 ) {
+		System.out.println("เกรดD");
+	}
+	else if (score >= 60 && score <= 69) {
+		System.out.println("เกรดC");
+	}
+	else if (score >= 70 && score <= 79) {
+		System.out.println("เกรดB");
+	}
+	else if (score >= 80 && score <= 100) {
+		System.out.println("เกรด A");
+	}
+	getscore.close();
+	}
 }
