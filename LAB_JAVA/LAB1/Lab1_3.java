d3ca2c91fe264c2c3814dcc3eb68db342f8af03c
Bank <tanakhanluenam@gmail.com>
2025-12-02 03:06:57 +0700

    Update Lab1_3.java

--- LAB_JAVA/LAB1/Lab1_3.java
+++ LAB_JAVA/LAB1/Lab1_3.java
@@ -1,16 +1,20 @@
+package Lab1;
+
 import java.util.Scanner;
 
 public class Lab1_3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-    
-        short Number;
-        System.out.print("input Number = ");
-        Number = scanner.nextShort();
-
-        if (Number % 2 != 0)
-            System.out.println("Number is odd");
-        else
-            System.out.println("Number is even");
-    }
+	public static void main(String[] args) {
+		Scanner examinenumber = new Scanner(System.in);
+		
+		System.out.println("Enter Number : ");
+		int number = examinenumber.nextInt();
+		
+		if (number % 2 != 0) {
+			System.out.println("Odd");
+		}
+		else {
+			System.out.println("Even");
+		}
+		examinenumber.close();
+	}
 }
