9d2ef0e951834ac7265f6854ab895dc421058fcb
Bank <tanakhanluenam@gmail.com>
2025-12-02 16:25:09 +0700

    Update Lab1_1.java

--- LAB_JAVA/LAB1/Lab1_1.java
+++ LAB_JAVA/LAB1/Lab1_1.java
@@ -1,16 +1,18 @@
-package Lab2;
+package Lab1;
 import java.util.Scanner;
 
-public class Lab2_1 {
+public class Lab1_1 {
 	public static void main(String[] args) {
 		Scanner getnumber = new Scanner(System.in);
-		System.out.println("Input number 1 : ");
-		int num1 = getnumber.nextInt();
 		
-		System.out.println("Input number 2 : ");
-		int num2 = getnumber.nextInt();
+		System.out.print("Enter you number1 : ");
+		int number1 = getnumber.nextInt();
 		
-		int sum = num1 + num2;
-		System.out.println("Your Number 1 + Number 2 = " + sum);
-	}
+		System.out.print("Enter you number2 : ");
+		int number2 = getnumber.nextInt();
+		
+		int sum = number1 + number2;
+		System.out.println(number1 + " + " + number2 + " = " + sum);
+
+		}
 }
