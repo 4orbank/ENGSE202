f5a179350dc744261073d2c07e5f56c7c429c6ec
Bank <tanakhanluenam@gmail.com>
2025-12-02 16:18:07 +0700

    Update Lab1_3.java

--- LAB_JAVA/LAB1/Lab1_3.java
+++ LAB_JAVA/LAB1/Lab1_3.java
@@ -1,11 +1,12 @@
 package Lab1;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Lab1_3 {
 	public static void main(String[] args) {
 		Scanner examinenumber = new Scanner(System.in);
-		
+	try {
 		System.out.println("Enter Number : ");
 		int number = examinenumber.nextInt();
 		
@@ -15,6 +16,11 @@ public class Lab1_3 {
 		else {
 			System.out.println("Even");
 		}
+		
+	} catch (InputMismatchException e) {
+        System.out.println("Error: Please enter a valid number.");
+    }
+	
 		examinenumber.close();
 	}
 }
