33232448b314453de2e2b71eb26f034348a3bae1
Bank <tanakhanluenam@gmail.com>
2025-12-02 16:20:19 +0700

    Update Lab1_2.java

--- LAB_JAVA/LAB1/Lab1_2.java
+++ LAB_JAVA/LAB1/Lab1_2.java
@@ -1,19 +1,25 @@
 package Lab1;
+
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Lab1_2 {
 	public static void main(String[] args) {
 		Scanner getnumber = new Scanner(System.in);
-		
-		System.out.println("Input Width : ");
+	try {
+		System.out.print("Input Width : ");
 		float width = getnumber.nextFloat();
 		
-		System.out.println("Input Height : ");
+		System.out.print("Input Height : ");
 		float height = getnumber.nextFloat();
 		
 		float area = width * height;
-		System.out.println("Your Area =" + area);
+		System.out.print("Your Area =" + area);
 
+	}catch (InputMismatchException e) {
+        System.out.print("Error: Please enter a valid number.");
+    }
+	
+	getnumber.close();
 	}
 }
