7db4f9c9ec5e38d25dabf7b9bc4ca022cf0b2017
Bank <tanakhanluenam@gmail.com>
2025-12-02 16:21:37 +0700

    Update lab1_5.java

--- LAB_JAVA/LAB1/lab1_5.java
+++ LAB_JAVA/LAB1/lab1_5.java
@@ -1,35 +1,30 @@
 package Lab1;
 import java.util.Scanner;
+
 public class Lab1_5 {
-	public static void main(String[] args){
-		
-		System.out.println("1 = Americano");
-		System.out.println("2 = Latte");
-		System.out.println("3 = Espresso");
-		System.out.println("4 = Mocha");
-		
-		System.out.print("Enter you menu : ");
-		Scanner menu = new Scanner(System.in);
-		
-		int memuchoice = menu.nextInt();
-		
-		
-		if (memuchoice < 1 || memuchoice > 4) {
+	public static void main(String[] args) {
+        Scanner getmenu = new Scanner(System.in);
+
+        System.out.println("1 = Americano");
+        System.out.println("2 = Latte");
+        System.out.println("3 = Espresso");
+        System.out.println("4 = Mocha");
+        System.out.print("Enter your menu choice: ");
+
+        int choicemenu = getmenu.nextInt();
+
+        if (choicemenu < 1 || choicemenu > 4) {
             System.out.println("Error: Invalid Menu");
-        }
-		else if (memuchoice == 1) {
+        } else if (choicemenu == 1) {
             System.out.println("You menu is Americano");
-        }
-        else if (memuchoice == 2) {
+        } else if (choicemenu == 2) {
             System.out.println("You menu is Latte");
-        }
-        else if (memuchoice == 3) {
+        } else if (choicemenu == 3) {
             System.out.println("You menu is Espresso");
-        }
-        else if (memuchoice == 4) {
+        } else if (choicemenu == 4) {
             System.out.println("You menu is Mocha");
         }
 
-		menu.close();
-	}
+        getmenu.close();
+    }
 }
